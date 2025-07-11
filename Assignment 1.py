b6c6c85074ae10ed6699c335363cdcaed52475a8
GutMuscle <princedogra350@gmail.com>
2025-07-11 18:29:30 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+
+#Task 1: Perform Basic Mathematical Operations
+a = float(input('Enter the first no. :'))
+b = float(input('Enter the second no. :'))
+print('Addition :', a+b)
+print('Subtraction :', a-b)
+print('Multiplication :', a*b)
+print('Division :', a/b)
+
+#Task 2: Create a Personalized Greeting
+First_name = (input('Enter your first name :'))
+Last_name = (input('Enter your last name :'))
+print('Hello,',First_name + Last_name+'!','Welcome to the Python Program.')
